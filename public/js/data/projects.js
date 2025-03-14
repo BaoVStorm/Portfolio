d2cb502e8cf9fa84c513c32a84f9b93ca01b9461
VStorm <tranvubao2004@gmail.com>
2025-03-15 02:07:53 +0700

    15/3/2025

--- public/js/data/projects.js
+++ public/js/data/projects.js
@@ -2,22 +2,28 @@ var url_img = "assets/images/";
 
 list_projects = [
     {
-        "img": "https://img.freepik.com/free-vector/virtual-currency-flat-style-banners-set_1284-15605.jpg",
-        "title": "Inversion Attack",
-        "des": "Inversion Attack is an attack technique in the field of security and machine learning, aimed at reconstructing the original data from the output or weights of an AI model.",
-        "href": "https://github.com/BaoVStorm/InversionAttack"
+        "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
+        "title": "Game King The Lost",
+        "des": "...",
+        "href": "https://github.com/BaoVStorm/King_The_Lost"
     },
     {
-        "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
-        "title": "Web_IntroduceMySelf",
-        "des": "...",
+        "img": "https://github.com/user-attachments/assets/dfcbef73-53d5-44e7-bf85-f8978d1c09d6",
+        "title": "My Portfolio Website",
+        "des": "This is my personal project — a website to introduce myself. Additionally, it serves as a platform to showcase my skills and continuously improve them through practice.",
         "href": "https://github.com/BaoVStorm/Web_IntroduceMySelf"
     },
     {
-        "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
-        "title": "Web Coconerd",
-        "des": "...",
+        "img": "https://static.vecteezy.com/system/resources/previews/055/600/847/non_2x/an-eye-catching-isometric-icon-of-a-plant-in-a-decorative-square-planter-vector.jpg",
+        "title": "Plant eCommerce Website",
+        "des": "This is a team project with my homies at <i>COCONERD</i> — an eCommerce website designed to manage and sell plants, featuring a streamlined process and a user-friendly interface for a smooth shopping experience.",
         "href": "https://github.com/coconerd/plant-ecommerce"
+    },
+    {
+        "img": "https://raw.githubusercontent.com/coconerd/uiteco-swing/refs/heads/main/src/main/resources/UITeco-funlogo.png",
+        "title": "UITeco Application",
+        "des": "A team project with COCONERD—my best team in university. This project is about an app that serves not only as a forum but also as a bridge connecting teachers, alumni, and students.",
+        "href": "https://github.com/coconerd/uiteco-swing"
     },
     {
         "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
@@ -32,16 +38,10 @@ list_projects = [
         "href": "https://github.com/BaoVStorm/Project_DataMining"
     },
     {
-        "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
-        "title": "Game King The Lost",
-        "des": "...",
-        "href": "https://github.com/BaoVStorm/King_The_Lost"
-    },
-    {
-        "img": "https://demo.cocobasic.com/fabius-wp/demo-1/wp-content/uploads/2020/11/portfolio_item_1.jpg",
-        "title": "Uiteco swing",
-        "des": "...",
-        "href": "https://github.com/coconerd/uiteco-swing"
+        "img": "https://img.freepik.com/free-vector/virtual-currency-flat-style-banners-set_1284-15605.jpg",
+        "title": "Inversion Attack",
+        "des": "Inversion Attack is an attack technique in the field of security and machine learning, aimed at reconstructing the original data from the output or weights of an AI model.",
+        "href": "https://github.com/BaoVStorm/InversionAttack"
     }
 ];
 
